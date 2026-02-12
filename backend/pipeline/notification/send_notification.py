47be8b6cb9fc3b0bd2fbb43de20893571ef54a26
Jonathan Zybert <jmzybert@gmail.com>
2026-02-12 11:43:24 -0800

    [GOO-39] Create function to POST payload notification (#9)

---
+++ backend/pipeline/notification/send_notification.py
@@ -0,0 +1,45 @@
+import base64
+import json
+import logging
+import os
+
+import functions_framework
+import requests
+from cloudevents.http.event import CloudEvent
+
+logger = logging.getLogger(__name__)
+
+POST_TIMEOUT_SECONDS = 5
+
+
+@functions_framework.cloud_event
+def send_notification(cloud_event: CloudEvent) -> None:
+    ENDPOINT = os.environ.get("ENDPOINT")
+
+    try:
+        if not ENDPOINT:
+            msg = "ENDPOINT environment variable not set."
+            raise SystemError(msg)
+
+        # Pub/Sub data is base64 encoded inside the 'data' field.
+        # cloud_event.data['message']['data'] contains the actual payload.
+        base64_data = cloud_event.data["message"]["data"]
+        decoded_string = base64.b64decode(base64_data).decode("utf-8")
+        payload = json.loads(decoded_string)
+
+        # Send POST request to endpoint.
+        response = requests.post(
+            ENDPOINT,
+            data=json.dumps(payload),
+            headers={"Content-Type": "application/json"},
+            timeout=POST_TIMEOUT_SECONDS,
+        )
+
+        # Raise an exception for bad status codes
+        response.raise_for_status()
+
+        logger.info(f"Message sent successfully! Status code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        logger.exception(f"POST request failed: {e}")
+        raise
