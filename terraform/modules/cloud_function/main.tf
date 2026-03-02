176986974529bc35859c272042800e5ce0dff9aa
Jake Close <jake.s.close@gmail.com>
2026-03-02 15:54:58 -0800

    Create cloud function module for future use of rules eval service and notification service

---
+++ terraform/modules/cloud_function/main.tf
@@ -0,0 +1,50 @@
+# 1. Zip the Python code directory
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = var.source_dir
+  output_path = "/tmp/function-${var.function_name}.zip"
+}
+
+# 2. Upload the zip to a Google Cloud Storage bucket
+resource "google_storage_bucket_object" "zip" {
+  name   = "${var.function_name}-${data.archive_file.source.output_md5}.zip"
+  bucket = var.source_bucket_name
+  source = data.archive_file.source.output_path
+}
+
+# 3. Create the Gen 2 Cloud Function
+resource "google_cloudfunctions2_function" "default" {
+  name        = var.function_name
+  location    = var.region
+  description = var.description
+
+  build_config {
+    runtime     = var.runtime
+    entry_point = var.entry_point 
+    source {
+      storage_source {
+        bucket = var.source_bucket_name
+        object = google_storage_bucket_object.zip.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count    = 1
+    available_memory      = "256M"
+    timeout_seconds       = 60
+    environment_variables = var.environment_variables
+  }
+
+  # If you pass a trigger_topic_id, it creates the Pub/Sub trigger. 
+  # If you don't, it defaults to a standard HTTP function.
+  dynamic "event_trigger" {
+    for_each = var.trigger_topic_id != null ? [var.trigger_topic_id] : []
+    content {
+      trigger_region = var.region
+      event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+      pubsub_topic   = event_trigger.value
+      retry_policy   = "RETRY_POLICY_DO_NOT_RETRY"
+    }
+  }
+}
---
+++ terraform/modules/cloud_function/variables.tf
@@ -0,0 +1,27 @@
+variable "function_name" { type = string }
+variable "region" { type = string }
+variable "entry_point" { type = string }
+variable "source_dir" { type = string }
+variable "source_bucket_name" { type = string }
+
+variable "description" { 
+  type    = string 
+  default = "Deployed via Terraform" 
+}
+
+variable "runtime" { 
+  type    = string 
+  default = "python311" 
+}
+
+variable "environment_variables" {
+  description = "A map of environment variables to pass to the function"
+  type        = map(string)
+  default     = {}
+}
+
+variable "trigger_topic_id" {
+  description = "The full ID of the Pub/Sub topic to trigger this function"
+  type        = string
+  default     = null
+}
