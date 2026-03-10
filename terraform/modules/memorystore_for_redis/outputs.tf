4616077842293cc91856dca26e2c57ced579e2be
Jonathan Zybert <jmzybert@gmail.com>
2026-03-10 12:52:06 -0700

    Add port to output.

--- terraform/modules/memorystore_for_redis/outputs.tf
+++ terraform/modules/memorystore_for_redis/outputs.tf
@@ -3,3 +3,8 @@ output "host" {
   description = "The IP address of the instance."
   value       = google_redis_instance.this.host
 }
+
+output "port" {
+  description = "The port of the instance."
+  value = google_redis_instance.this.port
+}
