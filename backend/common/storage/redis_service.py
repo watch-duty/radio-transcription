46d2b85356403e17e85b9d823141aa38602dac90
Jonathan Zybert <jmzybert@gmail.com>
2026-03-18 08:56:27 -0700

    [GOO-128] Integrate notifications with Redis instance (#86)

---
+++ backend/common/storage/redis_service.py
@@ -0,0 +1,42 @@
+import os
+
+from redis.backoff import ExponentialBackoff
+from redis.client import Redis
+from redis.exceptions import BusyLoadingError
+from redis.exceptions import ConnectionError as RedisConnectionError
+from redis.exceptions import TimeoutError as RedisTimeoutError
+from redis.retry import Retry
+
+from backend.common.storage.cache_provider import CacheProvider
+
+NUM_CONNECTION_RETRIES = 3
+DEFAULT_REDIS_PORT = "6379"
+REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")
+REDIS_PORT = int(os.environ.get("REDIS_PORT", DEFAULT_REDIS_PORT))
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
+
+
+class RedisService(CacheProvider):
+    """
+    Redis service object. Used to connect to a Redis instance.
+    Configured to retry on common connection errors.
+    """
+
+    def __init__(self) -> None:
+        retry = Retry(ExponentialBackoff(), NUM_CONNECTION_RETRIES)
+        self.client = Redis(
+            host=REDIS_HOST,
+            port=REDIS_PORT,
+            password=REDIS_PASSWORD,
+            db=0,
+            decode_responses=True,
+            retry=retry,
+            retry_on_error=[BusyLoadingError, RedisConnectionError, RedisTimeoutError],
+        )
+
+    def set_if_not_exists(self, key: str, value: str, ttl: int) -> bool:
+        """
+        Set a key/value pair in the Redis instance. Returns True if the key already exists,
+        or None if it does not.
+        """
+        return bool(self.client.set(key, value, nx=True, ex=ttl))
--- backend/pipeline/notification/send_notification.py
+++ backend/pipeline/notification/send_notification.py
@@ -8,6 +8,10 @@ import requests
 from cloudevents.http.event import CloudEvent
 from google.protobuf.json_format import MessageToJson
 
+from backend.common.storage.redis_service import RedisService
+from backend.pipeline.notification.notification_deduplication import (
+    NotificationDeduplication,
+)
 from backend.pipeline.schema_types.alert_notification_pb2 import AlertNotification
 from backend.pipeline.schema_types.evaluated_transcribed_audio_pb2 import (
     EvaluatedTranscribedAudio,
@@ -31,6 +35,11 @@ POST_TIMEOUT_SECONDS = 5
 
 NOTIFICATION_ENDPOINT = os.environ.get("NOTIFICATION_ENDPOINT")
 NOTIFICATION_ENDPOINT_API_KEY = os.environ.get("NOTIFICATION_ENDPOINT_API_KEY")
+
+# Keeping the notification deduplicate connection outside the main function. This is so the connection is
+# maintained while the function is warm instead of reconnecting each invocation.
+# TODO(schew): https://linear.app/watchduty/issue/GOO-173/update-local-dev-pipeline-with-redis
+deduplication = NotificationDeduplication(RedisService())
 
 
 def parse_cloud_event(cloud_event: CloudEvent) -> EvaluatedTranscribedAudio | None:
@@ -74,6 +83,12 @@ def send_notification(cloud_event: CloudEvent) -> None:
         alert_notification = convert_to_notification(evaluated_transcribed_audio)
         request_data = MessageToJson(alert_notification, indent=None)
         logger.info(f"Sending payload: {request_data}")
+
+        notification_id = alert_notification.transmission_id
+        if deduplication.process_notification(notification_id):
+            message = f"Duplicate transmission_id detected, skipping notification with ID: {notification_id}"
+            logger.warning(message)
+            return
 
         # Send POST request to endpoint.
         response = requests.post(
