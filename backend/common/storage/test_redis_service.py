caa5d292e98185b6ba09c164ccb81b63d6668358
Jonathan Zybert <jmzybert@gmail.com>
2026-03-19 09:42:48 -0700

    Add tests for Redis SSL.

--- backend/common/storage/test_redis_service.py
+++ backend/common/storage/test_redis_service.py
@@ -1,15 +1,18 @@
+import os
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import fakeredis
 
-from backend.common.storage.redis_service import RedisService
+with patch.dict(os.environ, {"REDIS_CERTIFICATE_PATH": "/secrets"}):
+    from backend.common.storage.redis_service import RedisService
 
 
 class TestRedisService(unittest.TestCase):
     def setUp(self) -> None:
+        self.mock_redis_factory = MagicMock(side_effect=fakeredis.FakeRedis)
         self.patcher = patch(
-            "backend.common.storage.redis_service.Redis", fakeredis.FakeRedis
+            "backend.common.storage.redis_service.Redis", self.mock_redis_factory
         )
         self.patcher.start()
         self.service = RedisService()
@@ -39,6 +42,26 @@ class TestRedisService(unittest.TestCase):
         self.assertFalse(result)
         self.assertEqual(self.service.client.get(key), value)
 
+    def test_ssl_configuration_is_passed(self) -> None:
+        self.mock_redis_factory.assert_called()
+
+        _, kwargs = self.mock_redis_factory.call_args
+
+        self.assertTrue(kwargs.get("ssl"), "SSL should be enabled")
+        self.assertEqual(kwargs.get("ssl_cert_reqs"), "required")
+        self.assertIn("/secrets/server_ca.pem", kwargs.get("ssl_ca_path", ""))
+
+    @patch.dict(os.environ, {"LOCAL_DEV": "True"})
+    def test_ssl_configuration_not_passed_in_local_dev(self) -> None:
+        self.service = RedisService()
+
+        self.mock_redis_factory.assert_called()
+
+        _, kwargs = self.mock_redis_factory.call_args
+
+        self.assertFalse(kwargs.get("ssl"), "SSL should not be enabled")
+        self.assertEqual(kwargs.get("ssl_cert_reqs"), "none")
+        self.assertIsNone(kwargs.get("ssl_ca_path"), "should be None")
 
 if __name__ == "__main__":
     unittest.main()
