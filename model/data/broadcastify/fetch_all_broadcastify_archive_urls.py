ce11e385e3330f7a4b1fb3b28b469fd2c9be8abf
Jake Close <jake.s.close@gmail.com>
2026-03-26 10:26:48 -0600

    feat: add optional trial API support to Broadcastify archive URL fetching script

--- model/data/broadcastify/fetch_all_broadcastify_archive_urls.py
+++ model/data/broadcastify/fetch_all_broadcastify_archive_urls.py
@@ -2,13 +2,14 @@ import csv
 import sys
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-from bcfy_api import fetch_archive_files
+from bcfy_api import fetch_archive_files, fetch_archive_days
 
 if len(sys.argv) < 2:
-    print("Usage: python sample_broadcastify_archive.py <feed_id>")
+    print("Usage: python fetch_all_broadcastify_archive_urls.py <feeds_csv> [--use-trial-api]")
     sys.exit(1)
 
 input_csv = str(sys.argv[1])
+use_trial_api = len(sys.argv) > 2 and sys.argv[2] == "--use-trial-api"
 
 # read the input csv. each row is: feed ID, feed name, is audio trimmed
 feeds = []
@@ -22,14 +23,17 @@ with open(input_csv) as f:
         feeds.append((feed_id, feed_name, is_audio_trimmed))
 
 
-def fetch_all_archives_for_feed(feed_id) -> list[str]:
-    return fetch_archive_files(feed_id)
-
+def fetch_all_archives_for_feed(feed_id: int, use_trial_api: bool = False) -> list[str]:
+    if use_trial_api:
+        return fetch_archive_files(feed_id)
+    else:
+        return fetch_archive_days(feed_id)
+    
 
 # use a threadpool to get archive URLs for all of the feeds
 with ThreadPoolExecutor(max_workers=10) as executor:
     future_to_feed = {
-        executor.submit(fetch_all_archives_for_feed, feed_id): (
+        executor.submit(fetch_all_archives_for_feed, feed_id, use_trial_api): (
             feed_id,
             feed_name,
             is_audio_trimmed,
