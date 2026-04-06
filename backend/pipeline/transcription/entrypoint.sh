de7239e0c3a548d8f11cf67ce4813fb6e059b078
arobee <arobee@google.com>
2026-04-06 20:19:21 +0000

    fix(transcription): use entrypoint script to support single image for Dataflow launcher and worker

---
+++ backend/pipeline/transcription/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Dataflow workers are invoked with specific flags like --logging_endpoint
+# If we see that flag, we know we are running as a worker.
+if [[ "$*" == *"--logging_endpoint"* ]]; then
+    echo "Running as Dataflow Worker..."
+    exec /opt/google/dataflow/boot "$@"
+else
+    echo "Running as Flex Template Launcher..."
+    exec /opt/google/dataflow/python_template_launcher "$@"
+fi
