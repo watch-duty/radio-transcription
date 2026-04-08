fdfce090df305364b2b4e378b35ba59ef343d285
Jonathan Zybert <jmzybert@gmail.com>
2026-04-08 16:46:52 -0700

    Reorder env exports

--- frontend/api/src/config.ts
+++ frontend/api/src/config.ts
@@ -3,19 +3,18 @@
  * alert at build time if an environment variable is missing.
  */
 
-export const TRANSCRIPTS_API_URL = process.env.TRANSCRIPTS_API_URL;
 export const WEB_URL = process.env.WEB_URL;
+export const TRANSCRIPTS_API_URL = process.env.TRANSCRIPTS_API_URL;
+export const RULES_API_URL = process.env.RULES_API_URL;
+
+if (!WEB_URL) {
+  throw new Error('WEB_URL environment variable is not set');
+}
 
 if (!TRANSCRIPTS_API_URL) {
   throw new Error('TRANSCRIPTS_API_URL environment variable is not set');
 }
 
-export const RULES_API_URL = process.env.RULES_API_URL;
-
 if (!RULES_API_URL) {
   throw new Error('RULES_API_URL environment variable is not set');
-}
-
-if (!WEB_URL) {
-  throw new Error('WEB_URL environment variable is not set');
 }
