36ad94d4a96d1925fbab05dc29e36c6af151bb6c
Jonathan Zybert <jmzybert@gmail.com>
2026-04-09 11:13:28 -0700

    [GOO-270] Add CORS support (#218)

--- frontend/api/src/config.ts
+++ frontend/api/src/config.ts
@@ -3,13 +3,17 @@
  * alert at build time if an environment variable is missing.
  */
 
+export const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN;
 export const TRANSCRIPTS_API_URL = process.env.TRANSCRIPTS_API_URL;
+export const RULES_API_URL = process.env.RULES_API_URL;
+
+if (!ALLOWED_ORIGIN) {
+  throw new Error('ALLOWED_ORIGIN environment variable is not set');
+}
 
 if (!TRANSCRIPTS_API_URL) {
   throw new Error('TRANSCRIPTS_API_URL environment variable is not set');
 }
-
-export const RULES_API_URL = process.env.RULES_API_URL;
 
 if (!RULES_API_URL) {
   throw new Error('RULES_API_URL environment variable is not set');
