d57ee6e31ee9e375ac49cd332b68925ebd77b3c9
Jake Close <jake.s.close@gmail.com>
2026-04-13 12:36:21 -0700

    feat: add descriptive comments to source_feed_id field in FeedCreate model

--- backend/services/feeds/models.py
+++ backend/services/feeds/models.py
@@ -13,6 +13,11 @@ class FeedBase(BaseModel):
 
 
 class FeedCreate(FeedBase):
+    # ID for each given data source.
+    # For Broadcastify Feeds, it would be the last part of
+    # https://partner.broadcastify.com/<FEED_ID>.
+    # For Echo, it would be the first part of
+    # <CHANNEL_NAME>/20260406/Santa_Clara_Co_Fire_Disp_20260406_102306.mp3.
     source_feed_id: str
     external_id: str
 
