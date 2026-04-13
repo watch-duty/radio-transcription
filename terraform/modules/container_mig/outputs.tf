a20a510e8925cec6df8efe600f2e7e0626d53565
Shuojing Wang <shuojing.wang@watchduty.org>
2026-04-13 13:26:47 -0700

    [GOO-286] Convert container_mig from zonal to regional MIG (#237)

--- terraform/modules/container_mig/outputs.tf
+++ terraform/modules/container_mig/outputs.tf
@@ -1,11 +1,11 @@
 output "instance_group_manager_id" {
   description = "The full resource ID of the managed instance group."
-  value       = google_compute_instance_group_manager.this.id
+  value       = google_compute_region_instance_group_manager.this.id
 }
 
 output "instance_group_self_link" {
   description = "The self-link of the instance group (for use in other resources)."
-  value       = google_compute_instance_group_manager.this.instance_group
+  value       = google_compute_region_instance_group_manager.this.instance_group
 }
 
 output "instance_template_id" {
