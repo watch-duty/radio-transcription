4ead04c7662339e1ac7fbbc0a35862a2521b1a20
Jonathan Zybert <jmzybert@gmail.com>
2026-04-14 11:37:30 -0700

    [GOO-282] Add react router with simple routing (#248)

--- frontend/transcription-ui/src/App.tsx
+++ frontend/transcription-ui/src/App.tsx
@@ -1,4 +1,8 @@
+import { Route, Routes } from 'react-router';
+
 import AppContainer from './components/AppContainer';
+import FeedsView from './components/feeds/FeedsView';
+import RulesView from './components/rules/RulesView';
 import TranscriptView from './components/transcripts/TranscriptView';
 import { useAuth } from './context/AuthContext';
 
@@ -7,10 +11,19 @@ import './App.css';
 function App() {
   const { token } = useAuth();
 
+  if (!token) {
+    return <AppContainer>Please login to continue.</AppContainer>;
+  }
+
+  // Define the application routes below.
   return (
-    <>
-      <AppContainer>{token && <TranscriptView />}</AppContainer>
-    </>
+    <AppContainer>
+      <Routes>
+        <Route path="/" element={<TranscriptView />} />
+        <Route path="/rules" element={<RulesView />} />
+        <Route path="/feeds" element={<FeedsView />} />
+      </Routes>
+    </AppContainer>
   );
 }
 
