6e5fdb4c71badc7cfdd9908a2a4e57fd0194c225
Jonathan Zybert <jmzybert@gmail.com>
2026-04-21 11:00:39 -0700

    Add comment for getAudioUrl

--- frontend/transcription-ui/src/utils/audioUtils.ts
+++ frontend/transcription-ui/src/utils/audioUtils.ts
@@ -1,3 +1,15 @@
+/**
+ * Converts a Google Cloud Storage URI to a URL that can be used by the browser.
+ *
+ * Most audio loading libraries cannot load from a `gs://` URI. Google provides a public endpoint
+ * for reading public GCS objects, which we can use here.
+ *
+ * For localhost development, we use a proxy endpoint to avoid CORS issues, which is `/gcs/*`.
+ * The vite config sets this up to proxy to `https://storage.googleapis.com/`.
+ *
+ * @param uri The Google Cloud Storage URI to convert.
+ * @returns The URL that can be used by the browser.
+ */
 export function getAudioUrl(uri: string): string {
   const base = import.meta.env.DEV
     ? '/gcs/'
