6dcbbcd789e5fe23437c43fe0f327ad057610759
Jonathan Zybert <jmzybert@gmail.com>
2026-04-22 09:45:57 -0700

    Fix format

--- terraform/modules/gcs_bucket/main.tf
+++ terraform/modules/gcs_bucket/main.tf
@@ -15,8 +15,8 @@ resource "google_storage_bucket" "this" {
     for_each = var.cors
     
     content {
-      origin        = cors.value.origin
-      method        = cors.value.method
+      origin          = cors.value.origin
+      method          = cors.value.method
       response_header = cors.value.response_header
       max_age_seconds = cors.value.max_age_seconds
     }
