16540c01b7f37fe2fa18b75f7e31837c6692946b
Jonathan Zybert <jmzybert@gmail.com>
2026-04-28 14:06:31 -0700

    Use MUI SignIn component

--- frontend/transcription-ui/src/components/Login.tsx
+++ frontend/transcription-ui/src/components/Login.tsx
@@ -1,15 +1,15 @@
 import { useNavigate } from 'react-router';
 
-import GoogleIcon from '@mui/icons-material/Google';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 import { useGoogleLogin } from '@react-oauth/google';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { SignInPage } from '@toolpad/core/SignInPage';
 
 import { useAuth } from '../context/AuthContext';
 import { authLogin } from '../service/authLogin';
 
 export function Login() {
+  const theme = useTheme();
   const navigate = useNavigate();
   const { setToken } = useAuth();
 
@@ -28,51 +28,12 @@ export function Login() {
   });
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        width: '100vw',
-      }}
-    >
-      <Box
-        sx={{
-          textAlign: 'center',
-          width: '20%',
-          borderRadius: '4px',
-          padding: 2,
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 1,
-          boxShadow:
-            'rgba(0, 0, 0, 0.2) 0px 2px 4px -1px, rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px',
-          border: '1px solid rgba(189, 189, 189, 0.4)',
-        }}
-      >
-        <Typography variant="h5">
-          <b>Sign in</b>
-        </Typography>
-        <Typography variant="subtitle1">
-          Please sign in to view radio transcriptions
-        </Typography>
-        <Button
-          variant="outlined"
-          color="primary"
-          startIcon={<GoogleIcon />}
-          onClick={() => login()}
-          sx={{
-            textTransform: 'none',
-            borderRadius: '4px',
-            padding: 1,
-            fontWeight: 600,
-          }}
-        >
-          Sign in with Google
-        </Button>
-      </Box>
-    </Box>
+    <AppProvider theme={theme}>
+      <SignInPage
+        providers={[{ id: 'google', name: 'Google' }]}
+        signIn={login}
+      />
+    </AppProvider>
   );
 }
 
