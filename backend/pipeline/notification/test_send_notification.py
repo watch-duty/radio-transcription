c532d95123204ac9dc7afcdf103452e8069ae612
Jonathan Zybert <jmzybert@gmail.com>
2026-05-14 10:49:14 -0700

    Remove duration field and send all notification payload keys even if empty

--- backend/pipeline/notification/test_send_notification.py
+++ backend/pipeline/notification/test_send_notification.py
@@ -80,7 +80,7 @@ class TestSendNotification(TestCase):
             source_audio_uris=["gs://foo/bar.flac"],
             feed_name="asdf",
             external_id="ext-id",
-            app_url="https://app.example.com?feedId=&transmissionId=1234&timestamp=1000000&duration=5",
+            app_url="https://app.example.com?feedId=&transmissionId=1234&timestamp=1000000",
         )
         expected_notification.start_audio_offset.seconds = 10
         expected_notification.end_audio_offset.seconds = 20
@@ -177,7 +177,7 @@ class TestSendNotification(TestCase):
         self.assertEqual(
             notification.app_url,
             "https://app.example.com?feedId=feed-1&transmissionId=tx-1"
-            "&timestamp=1776280988990&duration=5",
+            "&timestamp=1776280988990",
         )
         self.assertEqual(notification.start_timestamp.seconds, 1776280988)
         self.assertEqual(notification.start_timestamp.nanos, 990000000)
