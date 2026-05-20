b9f3cd604082c090978b2bf59c936ad6d94201e1
schew <stephaniechew@google.com>
2026-05-20 22:54:05 +0000

    revert

--- integration_tests/e2e/test_rules_creation_evaluation_publish.py
+++ integration_tests/e2e/test_rules_creation_evaluation_publish.py
@@ -85,7 +85,7 @@ def test_rules_creation_evaluation_publish(
 
     create_test_rule(unique_keyword)
     # Wait for rules evaluation cache to expire (~60s)
-    time.sleep(65)
+    # time.sleep(65)
     feed_id, _ = test_bcfy_feed
     publish_test_message(unique_trans_id, unique_transcript, feed_id)
 
@@ -117,6 +117,6 @@ def test_rules_creation_evaluation_publish(
 
     assert_eventually(
         transcript_and_notification_received,
-        timeout_sec=120.0,
+        timeout_sec=70.0,
         error_msg=f"Did not receive expected notification or transcript matching {unique_trans_id}.",
     )
