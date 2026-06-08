f4dcadbe4f7d3944fd6cb054867d8eb13aa2041a
Rob Moore <rob.moore@gmail.com>
2026-06-08 11:35:47 -0500

    style: regenerate protobuf python schemas

--- backend/pipeline/schema_types/streaming_state.py
+++ backend/pipeline/schema_types/streaming_state.py
@@ -55,7 +55,7 @@ class FlushRequestProto(betterproto.Message):
     session_id: str = betterproto.string_field(3)
     contributing_audio_uris: List[str] = betterproto.string_field(4)
     time_range: "TimeRangeProto" = betterproto.message_field(5)
-    transmission_id: str = betterproto.string_field(6)
+    segment_id: str = betterproto.string_field(6)
     feed_metadata: "FeedMetadataProto" = betterproto.message_field(7)
     sample_rate: int = betterproto.int32_field(8)
     missing_prior_context: bool = betterproto.bool_field(9)
@@ -70,6 +70,7 @@ class FlushRequestProto(betterproto.Message):
     traceparent: Optional[str] = betterproto.string_field(
         14, optional=True, group="_traceparent"
     )
+    audio_classification: int = betterproto.int32_field(15)
 
 
 @dataclass(eq=False, repr=False)
