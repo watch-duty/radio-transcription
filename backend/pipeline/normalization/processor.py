e5009fbe832658b6c573550b9891c4a2f396720f
Jake Close <jake.s.close@gmail.com>
2026-06-11 11:59:19 -0700

    fix: resolve syntax error in normalization processor

--- backend/pipeline/normalization/processor.py
+++ backend/pipeline/normalization/processor.py
@@ -306,6 +306,7 @@ class NormalizationEventProcessor:
             feed_name=segmented_audio.feed_name,
             audio_classification=SegmentedAudio.AudioClassification.Name(
                 segmented_audio.audio_classification
+            ),
             transcription_audio_uri=transcription_audio_uri,
             ingested_at=segmented_audio.ingested_at,
         )
