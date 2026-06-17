fe1b53c10fd0a2b5ecce5122ca7fc0cfe8d28a9a
ax0420 <anthony.xiang@watchduty.org>
2026-06-17 18:43:43 +0000

    [GOO-517] refactor(database): consolidate audio_segments indexes

--- terraform/modules/alloydb/sql/ingestion/028_audio_segments_speech_index.sql
+++
@@ -1,4 +0,0 @@
--- Create a partial index on audio_segments for SPEECH classification to optimize feed timestamp querying.
-CREATE INDEX IF NOT EXISTS idx_audio_segments_last_speech
-    ON audio_segments (feed_id, end_timestamp DESC, id DESC)
-    WHERE classification = 'SPEECH';
