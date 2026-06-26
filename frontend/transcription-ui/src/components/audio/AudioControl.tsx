dadeeb86acf9f7eff498bd4d1979d2ee43eb7b60
Jonathan Zybert <jmzybert@gmail.com>
2026-06-26 16:02:14 -0700

    Fix always play checkbox disable state by combining disableCheckbox and disableControls

--- frontend/transcription-ui/src/components/audio/AudioControl.tsx
+++ frontend/transcription-ui/src/components/audio/AudioControl.tsx
@@ -177,12 +177,11 @@ export function AudioControl({
             <Checkbox
               checked={playLatestAudio}
               onChange={(e) => togglePlayLatestAudio(e.target.checked)}
-              disabled={disableCheckbox}
             />
           }
           label="Always play latest audio"
           slotProps={{ typography: { variant: 'body2' } }}
-          disabled={disableControls}
+          disabled={disableCheckbox || disableControls}
         />
       </Box>
     </Box>
