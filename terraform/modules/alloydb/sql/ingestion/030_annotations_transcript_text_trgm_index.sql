411419954d61f213d5342e551c55c3235f83a079
ax0420 <anthony.xiang@watchduty.org>
2026-06-30 14:53:07 +0000

    use gist index

--- terraform/modules/alloydb/sql/ingestion/030_annotations_transcript_text_trgm_index.sql
+++ terraform/modules/alloydb/sql/ingestion/030_annotations_transcript_text_trgm_index.sql
@@ -1,7 +1,7 @@
 -- Create pg_trgm extension if not exists for trigram index.
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
--- Create partial BRIN trigram index on transcript annotations text for fast substring searching under high write throughput.
-CREATE INDEX IF NOT EXISTS idx_annotations_transcript_text_brin
-ON annotations USING brin ((data->>'text') brin_trgm_ops)
+-- Create partial GiST trigram index on transcript annotations text for fast substring searching under high write throughput.
+CREATE INDEX IF NOT EXISTS idx_annotations_transcript_text_gist
+ON annotations USING gist ((data->>'text') gist_trgm_ops)
 WHERE type = 'TRANSCRIPT'::annotation_type;
