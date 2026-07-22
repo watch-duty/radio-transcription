9a5dc6f50bf3f18267013ed1955cb55e00359fa5
schew <stephaniechew@google.com>
2026-07-22 06:59:19 +0000

    migrate feeds api

---
+++ terraform/modules/services/api/feeds/outputs.tf
@@ -0,0 +1,14 @@
+output "feed_store_service_url" {
+  description = "The URL where the Feed Store Service is hosted"
+  value       = google_cloud_run_v2_service.feed_store.uri
+}
+
+output "feed_store_service_name" {
+  description = "The name of the service"
+  value       = google_cloud_run_v2_service.feed_store.name
+}
+
+output "feed_store_sa_email" {
+  description = "The email address of the Feed Store service account"
+  value       = google_service_account.feed_store_sa.email
+}
