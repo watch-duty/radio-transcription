125a603e21131889766fe0d1bd40b402c7cf0914
schew <stephaniechew@google.com>
2026-07-22 17:15:41 +0000

    rename

--- terraform/modules/services/api/rules/outputs.tf
+++ terraform/modules/services/api/rules/outputs.tf
@@ -1,14 +1,14 @@
 output "rules_api_url" {
   description = "The URL where the Rules API Service is hosted"
-  value       = google_cloud_run_v2_service.rules_management.uri
+  value       = google_cloud_run_v2_service.rules_api.uri
 }
 
 output "rules_api_service_name" {
   description = "The exact name of the service (used by GitHub Actions and IAM)"
-  value       = google_cloud_run_v2_service.rules_management.name
+  value       = google_cloud_run_v2_service.rules_api.name
 }
 
 output "rules_api_sa_email" {
-  description = "The email address of the rules management service account"
-  value       = google_service_account.rules_management_sa.email
+  description = "The email address of the rules API service account"
+  value       = google_service_account.rules_api_sa.email
 }
