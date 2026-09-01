add98ae426488a67fce907e4975dcc32b9e26ed0
Joshua Frederick <joshfrederick@watchduty.org>
2026-09-01 08:10:30 -0700

    feedback: add comment around unique index with reference

--- terraform/modules/alloydb/sql/ingestion/013_feed_properties_source_type.sql
+++ terraform/modules/alloydb/sql/ingestion/013_feed_properties_source_type.sql
@@ -9,5 +9,6 @@ BEGIN
         ALTER TABLE feed_properties ADD COLUMN source_type TEXT NOT NULL DEFAULT '';
     END IF;
 END $$;
+-- Temporarily dropped for a load test (#1017), restored by revert (#1080).
 CREATE UNIQUE INDEX IF NOT EXISTS idx_feed_properties_source_lookup
 ON feed_properties(source_type, source_feed_id);
